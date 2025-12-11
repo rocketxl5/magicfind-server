b37b5ef682363d00b9b6a96efbe70310e9fd85ae
Max Gaillard <maxgaillar@gmail.com>
2025-12-11 13:10:01 -0500

    suspended query health check to next-cms-template

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ app.listen(PORT, () => {
     dynoWaker.start();
     console.log(`Server started on port: ${PORT}`)
 
-    const NEXT_HEALTH_URL = 'https://next-cms-template.onrender.com/api/health';
+    // const NEXT_HEALTH_URL = 'https://next-cms-template.onrender.com/api/health';
 
     // Extra keep-alive ping
-    setInterval(() => {
-        axios.get(NEXT_HEALTH_URL)
-            .then(() => console.log('[KEEP-ALIVE] Pinged Next.js health route'))
-            .catch(err => console.error('[KEEP-ALIVE] Next.js ping failed:', err));
-    }, 10 * 60 * 1000); // 10 minutes
+    // setInterval(() => {
+    //     axios.get(NEXT_HEALTH_URL)
+    //         .then(() => console.log('[KEEP-ALIVE] Pinged Next.js health route'))
+    //         .catch(err => console.error('[KEEP-ALIVE] Next.js ping failed:', err));
+    // }, 10 * 60 * 1000); // 10 minutes
 });
